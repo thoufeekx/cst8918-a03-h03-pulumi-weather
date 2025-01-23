2c7ec8ed42a83019a177f7c8bda24aa6f07f1f87
thoufeekx <thoufeekbaber1@gmail.com>
2025-01-23 17:52:13 -0500

    Create infrstucture folder

---
+++ infrastructure/index.ts
@@ -0,0 +1,139 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as resources from "@pulumi/azure-native/resources";
+import * as containerregistry from "@pulumi/azure-native/containerregistry";
+
+
+// Other imports at the top of the module
+import * as docker from '@pulumi/docker'
+
+
+// container instane
+import * as containerinstance from '@pulumi/azure-native/containerinstance'
+
+// Import the configuration settings for the current stack.
+const config = new pulumi.Config()
+const appPath = config.require('appPath')
+const prefixName = config.require('prefixName')
+const imageName = prefixName
+const imageTag = config.require('imageTag')
+// Azure container instances (ACI) service does not yet support port mapping
+// so, the containerPort and publicPort must be the same
+const containerPort = config.requireNumber('containerPort')
+const publicPort = config.requireNumber('publicPort')
+const cpu = config.requireNumber('cpu')
+const memory = config.requireNumber('memory')
+
+// Retrieve configuration values
+// const config = new pulumi.Config();
+// const prefixName = config.require("prefixName");
+
+// Create a resource group
+const resourceGroup = new resources.ResourceGroup(`${prefixName}-rg`);
+
+
+// Create the container registry
+const registry = new containerregistry.Registry(`${prefixName}ACR`, {
+    resourceGroupName: resourceGroup.name,
+    adminUserEnabled: true,
+    sku: {
+        name: containerregistry.SkuName.Basic, // Cost-optimized registry tier
+    },
+});
+
+
+// Get the authentication credentials for the container registry.
+const registryCredentials = containerregistry
+  .listRegistryCredentialsOutput({
+    resourceGroupName: resourceGroup.name,
+    registryName: registry.name,
+  })
+  .apply((creds) => {
+    return {
+      username: creds.username!,
+      password: creds.passwords![0].value!,
+    }
+  })
+
+
+// export const acrServer = registry.loginServer
+// export const acrUsername = registryCredentials.username
+
+
+// Define the container image for the service.
+const image = new docker.Image(`${prefixName}-image`, {
+    imageName: pulumi.interpolate`${registry.loginServer}/${imageName}:${imageTag}`,
+    build: {
+      context: appPath,
+      platform: 'linux/amd64',
+    },
+    registry: {
+      server: registry.loginServer,
+      username: registryCredentials.username,
+      password: registryCredentials.password,
+    },
+  })
+
+
+
+  // Create a container group in the Azure Container App service and make it publicly accessible.
+const containerGroup = new containerinstance.ContainerGroup(
+    `${prefixName}-container-group`,
+    {
+      resourceGroupName: resourceGroup.name,
+      osType: 'linux',
+      restartPolicy: 'always',
+      imageRegistryCredentials: [
+        {
+          server: registry.loginServer,
+          username: registryCredentials.username,
+          password: registryCredentials.password,
+        },
+      ],
+      containers: [
+        {
+          name: imageName,
+          image: image.imageName,
+          ports: [
+            {
+              port: containerPort,
+              protocol: 'tcp',
+            },
+          ],
+          environmentVariables: [
+            {
+              name: 'PORT',
+              value: containerPort.toString(),
+            },
+            {
+              name: 'WEATHER_API_KEY',
+              value: '649f5df47e42d795f648c71f3ce03c8f',
+            },
+          ],
+          resources: {
+            requests: {
+              cpu: cpu,
+              memoryInGB: memory,
+            },
+          },
+        },
+      ],
+      ipAddress: {
+        type: containerinstance.ContainerGroupIpAddressType.Public,
+        dnsNameLabel: `${imageName}`,
+        ports: [
+          {
+            port: publicPort,
+            protocol: 'tcp',
+          },
+        ],
+      },
+    },
+  )
+
+
+  // Export the service's IP address, hostname, and fully-qualified URL.
+export const hostname = containerGroup.ipAddress.apply((addr) => addr!.fqdn!)
+export const ip = containerGroup.ipAddress.apply((addr) => addr!.ip!)
+export const url = containerGroup.ipAddress.apply(
+  (addr) => `http://${addr!.fqdn!}:${containerPort}`,
+)
